f666f74bc4601637f892f834941c281a110b7a46
Kenway-web <66524914+Kenway-web@users.noreply.github.com>
2024-01-07 20:59:12 +0530

    Initial Commit

---
+++ JP Predators/JP_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_PredatorsApp.swift
+//  JP Predators
+//
+//  Created by Shoaib Ali on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct JP_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
